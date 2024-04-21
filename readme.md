a4b0251b83a70ff2e702c5f4b6a8df2ed7fd748a
ItsLynix <lynixthewolf@gmail.com>
2024-04-21 02:38:00 +0200

    sidekick: initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Sidekick
+## Desktop Utility for managing ADB and Fastboot for Android devices.
+
+## Contributing
+
+*DONT. yet.*
---
+++ ui/js/material.js
@@ -0,0 +1,5 @@
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/iconbutton/filled-tonal-icon-button.js';
+import '@material/web/icon/icon.js';
+import '@material/web/checkbox/checkbox.js';
