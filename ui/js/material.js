6a6a3baaa3f6e1387c40cd8a924a5502263e44e8
ItsLynix <lynixthewolf@gmail.com>
2024-04-23 00:23:35 +0200

    material: use google-fonts

--- ui/js/material.js
+++ ui/js/material.js
@@ -1,5 +1,6 @@
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
 import '@material/web/iconbutton/filled-tonal-icon-button.js';
+import '@material/web/iconbutton/filled-icon-button.js';
 import '@material/web/icon/icon.js';
 import '@material/web/checkbox/checkbox.js';
